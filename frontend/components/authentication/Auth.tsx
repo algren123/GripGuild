11eb84e60e5d805ed22fe24db9c1c711be1af9f3
Algren-Michael Pauna <algren-michael.pauna@skybettingandgaming.com>
2024-02-22 15:25:37 +0000

    clean build

--- frontend/components/authentication/Auth.tsx
+++ frontend/components/authentication/Auth.tsx
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Alert, StyleSheet, View, AppState, Text } from "react-native";
-import { Button, Input } from "react-native-elements";
-import axios from "axios";
-import { useMutation } from "@tanstack/react-query";
+import { StyleSheet, View, Text } from "react-native";
 
 import {
   GoogleSignin,
@@ -19,9 +16,9 @@ export default function Auth() {
   const configureGoogleSignIn = () => {
     GoogleSignin.configure({
       webClientId:
-        "1062870520492-g9cdjtvgpmd0cchkk9mcm30fgnbjk0bq.apps.googleusercontent.com",
-      iosClientId:
-        "1062870520492-ggg23b1kal30sm9pijftshe18qa8v54u.apps.googleusercontent.com",
+        "1062870520492-sudg8c1bdpfnkjnglbv3u9gdi773ovrq.apps.googleusercontent.com",
+      // iosClientId:
+      //   "1062870520492-ggg23b1kal30sm9pijftshe18qa8v54u.apps.googleusercontent.com",
     });
   };
 
@@ -31,7 +28,6 @@ export default function Auth() {
 
   const signIn = async () => {
     try {
-      console.log("pressed");
       await GoogleSignin.hasPlayServices();
       const user = await GoogleSignin.signIn();
       setUserInfo(user);
@@ -65,118 +61,6 @@ export default function Auth() {
     </View>
   );
 }
-// const { mutateAsync: addUserMutation } = useMutation({
-//   mutationFn: async (newUser: User) => {
-//     console.log(newUser);
-//     const data = await axios.post(
-//       'https://0813-2a02-c7c-aed0-6000-edae-a4fe-24e1-7cd4.ngrok-free.app/api/users/signup',
-//       {
-//         name: displayName,
-//         email: newUser.email,
-//         supabaseUserId: newUser.id,
-//       }
-//     );
-
-//     console.log(data);
-//     return data;
-//   },
-//   onError: (error) => {
-//     console.error(error);
-//   },
-// });
-
-// async function signInWithEmail() {
-//   setLoading(true);
-//   const {
-//     error,
-//     data: { user },
-//   } = await supabase.auth.signInWithPassword({
-//     email: email,
-//     password: password,
-//   });
-
-//   console.log(user);
-//   if (error) Alert.alert(error.message);
-
-//   setLoading(false);
-// }
-
-// async function signUpWithEmail() {
-//   setLoading(true);
-//   const {
-//     data: { session, user },
-//     error,
-//   } = await supabase.auth.signUp({
-//     email: email,
-//     password: password,
-//   });
-
-//   if (error) Alert.alert(error.message);
-
-//   if (user) {
-//     try {
-//       await addUserMutation(user);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   }
-
-//   if (!session)
-//     Alert.alert('Please check your inbox for email verification!');
-
-//   setLoading(false);
-// }
-
-//   return (
-//     <View style={styles.container}>
-//       <View style={[styles.verticallySpaced, styles.mt20]}>
-//         <Input
-//           label="Display Name"
-//           leftIcon={{ type: 'font-awesome', name: 'envelope' }}
-//           onChangeText={(text) => setDisplayName(text)}
-//           value={displayName}
-//           placeholder="Boulderer123"
-//           autoCapitalize={'none'}
-//         />
-//       </View>
-//       <View style={[styles.verticallySpaced, styles.mt20]}>
-//         <Input
-//           label="Email"
-//           leftIcon={{ type: 'font-awesome', name: 'envelope' }}
-//           onChangeText={(text) => setEmail(text)}
-//           value={email}
-//           placeholder="email@address.com"
-//           autoCapitalize={'none'}
-//         />
-//       </View>
-//       <View style={styles.verticallySpaced}>
-//         <Input
-//           label="Password"
-//           leftIcon={{ type: 'font-awesome', name: 'lock' }}
-//           onChangeText={(text) => setPassword(text)}
-//           value={password}
-//           secureTextEntry={true}
-//           placeholder="Password"
-//           autoCapitalize={'none'}
-//         />
-//       </View>
-//       <View style={[styles.verticallySpaced, styles.mt20]}>
-//         <Button
-//           title="Sign in"
-//           disabled={loading}
-//           onPress={() => signInWithEmail()}
-//         />
-//       </View>
-//       <View style={styles.verticallySpaced}>
-//         <Button
-//           title="Sign up"
-//           disabled={loading}
-//           onPress={() => signUpWithEmail()}
-//         />
-//       </View>
-//     </View>
-//   );
-// }
 
 const styles = StyleSheet.create({
   container: {
